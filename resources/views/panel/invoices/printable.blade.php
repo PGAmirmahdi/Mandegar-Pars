e0b7a08abaf57f52cfb8f79927bfad4b9fbe9d96
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-06-16 11:34:35 +0330

    Fixed the bug of invoice when a factor submits for a customer(the bug was the stamp and sign when the factor was't released)

--- resources/views/panel/invoices/printable.blade.php
+++ resources/views/panel/invoices/printable.blade.php
@@ -332,9 +332,11 @@
                                     @endif
                                     <tr>
                                         <td colspan="6" id="seller_sign_sec">
+                                            @if($invoice->status == "invoiced")
                                             <img src="{{ $invoice->user->sign_image ?? '' }}" class="sign">
                                             @if($invoice->type == 'official')
                                             <img src="{{ asset('/assets/media/image/stamp.png') }}" class="stamp">
+                                            @endif
                                             @endif
                                             <small>مهر و امضای فروشنده</small>
                                         </td>
