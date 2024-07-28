5f495fb48ddbdf47d44f0a094caf812553bdafc9
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-28 16:05:23 +0330

    fixed changes

--- resources/views/sms/index.blade.php
+++ resources/views/sms/index.blade.php
@@ -17,13 +17,13 @@
                 </div>
                 <form action="{{ route('sms.search') }}" method="get" id="search_form"></form>
                 <div class="row mb-3">
-                    <div class="col-xl-2 xl-lg-2 col-md-3 col-sm-12">
-                        <input type="text" name="receiver_name" class="form-control" placeholder="نام گیرنده" value="{{ request()->receiver_name ?? null }}" form="search_form">
+                    <div class="col-xl-2 col-lg-2 col-md-3 col-sm-12">
+                        <input type="text" name="receiver_name" class="form-control" placeholder="نام گیرنده" value="{{ request()->receiver_name ?? '' }}" form="search_form">
                     </div>
-                    <div class="col-xl-3 xl-lg-3 col-md-4 col-sm-12">
-                        <input type="text" name="receiver_phone" class="form-control" placeholder="شماره گیرنده" value="{{ request()->receiver_phone ?? null }}" form="search_form">
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-12">
+                        <input type="text" name="receiver_phone" class="form-control" placeholder="شماره گیرنده" value="{{ request()->receiver_phone ?? '' }}" form="search_form">
                     </div>
-                    <div class="col-xl-2 xl-lg-2 col-md-3 col-sm-12">
+                    <div class="col-xl-2 col-lg-2 col-md-3 col-sm-12">
                         <button type="submit" class="btn btn-primary" form="search_form">جستجو</button>
                     </div>
                 </div>
@@ -35,9 +35,10 @@
                             <th>نام گیرنده</th>
                             <th>شماره گیرنده</th>
                             <th>پیام</th>
+                            <th>وضعیت</th>
                             <th>تاریخ ایجاد</th>
                             @can('sms-show')
-                            <th>مشاهده</th>
+                                <th>مشاهده</th>
                             @endcan
                             @can('sms-delete')
                                 <th>حذف</th>
@@ -47,18 +48,18 @@
                         <tbody>
                         @foreach($smsList as $key => $sms)
                             <tr>
-                                <td>{{ ++$key }}</td>
+                                <td>{{ $key + 1 }}</td>
                                 <td>{{ $sms->receiver_name }}</td>
                                 <td>{{ $sms->receiver_phone }}</td>
                                 <td>{{ $sms->message }}</td>
                                 <td>{{ $sms->status }}</td>
                                 <td>{{ verta($sms->created_at)->format('H:i - Y/m/d') }}</td>
                                 @can('sms-show')
-                                <td>
-                                    <a class="btn btn-info btn-floating" href="{{ route('sms.show', $sms->id) }}">
-                                        <i class="fa fa-eye"></i>
-                                    </a>
-                                </td>
+                                    <td>
+                                        <a class="btn btn-info btn-floating" href="{{ route('sms.show', $sms->id) }}">
+                                            <i class="fa fa-eye"></i>
+                                        </a>
+                                    </td>
                                 @endcan
                                 @can('sms-delete')
                                     <td>
@@ -72,11 +73,13 @@
                         </tbody>
                         <tfoot>
                         <tr>
+                            <td colspan="8">
+                                <div class="d-flex justify-content-center">{{ $smsList->appends(request()->all())->links() }}</div>
+                            </td>
                         </tr>
                         </tfoot>
                     </table>
                 </div>
-                <div class="d-flex justify-content-center">{{ $smsList->appends(request()->all())->links() }}</div>
             </div>
         </div>
     @endcan
