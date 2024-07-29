27d8f1184e98f04b3fb6e856eebb70a15c087845
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-29 11:28:31 +0330

    fixed some changes

--- resources/views/panel/layouts/sidebar.blade.php
+++ resources/views/panel/layouts/sidebar.blade.php
@@ -29,7 +29,7 @@
                 </li>
             @endcanany
             @canany(['customers-list','foreign-customers-list'])
-                <li class="{{ active_sidebar(['customers','customers/create','customers/{customer}/edit','search/customers','foreign-customers','foreign-customers/create','foreign-customers/{foreign_customer}/edit','search/foreign-customers','sms','sms/create','sms/{sms}/edit','sms.search']) ? 'active' : '' }}" data-toggle="tooltip" title="مشتریان">
+                <li class="{{ active_sidebar(['customers','customers/create','customers/{customer}/edit','search/customers','foreign-customers','foreign-customers/create','foreign-customers/{foreign_customer}/edit','search/foreign-customers','sms','sms/create','sms/{sms}/edit','sms.search','sms/{sms}/show]) ? 'active' : '' }}" data-toggle="tooltip" title="مشتریان">
                     <a href="#navigationCustomers" title="مشتریان">
                         <i class="icon ti-user"></i>
                     </a>
@@ -217,7 +217,7 @@
                 </li>
             @endcan
         </ul>
-        <ul id="navigationCustomers" class="{{ active_sidebar(['customers','customers/create','customers/{customer}/edit','search/customers','foreign-customers','foreign-customers/create','foreign-customers/{foreign_customer}/edit','search/foreign-customers','sms','sms/create','sms/{sms}/edit','sms.search']) ? 'navigation-active' : '' }}">
+        <ul id="navigationCustomers" class="{{ active_sidebar(['customers','customers/create','customers/{customer}/edit','search/customers','foreign-customers','foreign-customers/create','foreign-customers/{foreign_customer}/edit','search/foreign-customers','sms','sms/create','sms/{sms}/edit','sms.search','sms/{sms}/show]) ? 'navigation-active' : '' }}">
             <li class="navigation-divider">مشتریان</li>
             @can('customers-list')
                 <li>
@@ -231,7 +231,7 @@
             @endcan
             @can('sms-list')
                 <li>
-                    <a class="{{ active_sidebar(['sms','sms/create']) ? 'active' : '' }}" href="{{ route('sms.index') }}">پنل پیامکی</a>
+                    <a class="{{ active_sidebar(['sms','sms/create','sms/{sms}/show']) ? 'active' : '' }}" href="{{ route('sms.index') }}">پنل پیامکی</a>
                 </li>
             @endcan
         </ul>
