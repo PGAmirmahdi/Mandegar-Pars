86679b59163d1c1b94ecab7c6c47afc9a150a4c8
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-31 10:50:34 +0330

    fixed

--- app/Http/Controllers/Panel/SMSController.php
+++ app/Http/Controllers/Panel/SMSController.php
@@ -15,7 +15,7 @@ class SMSController extends Controller
     public function index()
     {
         $this->authorize('sms-list');
-        $smsList = Sms::query()->orderby('id')->paginate(10);
+        $smsList = Sms::query()->orderByDesc('id')->paginate(10);
 
         return view('panel.sms.index', compact('smsList'));
     }
