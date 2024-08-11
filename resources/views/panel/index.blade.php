33c5c82c3dd260f3215b062bbbfd32bc760308d0
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-08-11 15:35:16 +0330

    fixed

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -33,112 +33,112 @@
     @endif
     <div class="card">
         <div class="card-body">
-                <div class="card-title d-flex justify-content-between align-items-center">
-                    <h6>آمار</h6>
-                    <div class="slick-single-arrows">
-                        <a class="btn btn-outline-light btn-sm">
-                            <i class="ti-angle-right"></i>
-                        </a>
-                        <a class="btn btn-outline-light btn-sm">
-                            <i class="ti-angle-left"></i>
-                        </a>
+            <div class="card-title d-flex justify-content-between align-items-center">
+                <h6>آمار</h6>
+                <div class="slick-single-arrows">
+                    <a class="btn btn-outline-light btn-sm">
+                        <i class="ti-angle-right"></i>
+                    </a>
+                    <a class="btn btn-outline-light btn-sm">
+                        <i class="ti-angle-left"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="row slick-single-item">
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                    <div class="card border mb-0">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <div>
+                                    <div class="icon-block icon-block-sm bg-success icon-block-floating mr-2">
+                                        <i class="fa fa-users"></i>
+                                    </div>
+                                </div>
+                                <span class="font-size-13">کاربران</span>
+                                <h2 class="mb-0 ml-auto font-weight-bold text-success primary-font line-height-30">{{ \App\Models\User::count() }}</h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            <div class="row slick-single-item">
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-                            <div class="card border mb-0">
-                                <div class="card-body">
-                                    <div class="d-flex align-items-center mb-3">
-                                        <div>
-                                            <div class="icon-block icon-block-sm bg-success icon-block-floating mr-2">
-                                                <i class="fa fa-users"></i>
-                                            </div>
-                                        </div>
-                                        <span class="font-size-13">کاربران</span>
-                                        <h2 class="mb-0 ml-auto font-weight-bold text-success primary-font line-height-30">{{ \App\Models\User::count() }}</h2>
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                    <div class="card border mb-0">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <div>
+                                    <div class="icon-block icon-block-sm bg-secondary icon-block-floating mr-2">
+                                        <i class="fa fa-users"></i>
                                     </div>
                                 </div>
+                                <span class="font-size-13">مشتریان</span>
+                                <h2 class="mb-0 ml-auto font-weight-bold text-secondary primary-font line-height-30">{{ \App\Models\Customer::count() }}</h2>
                             </div>
                         </div>
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-                            <div class="card border mb-0">
-                                <div class="card-body">
-                                    <div class="d-flex align-items-center mb-3">
-                                        <div>
-                                            <div class="icon-block icon-block-sm bg-secondary icon-block-floating mr-2">
-                                                <i class="fa fa-users"></i>
-                                            </div>
-                                        </div>
-                                        <span class="font-size-13">مشتریان</span>
-                                        <h2 class="mb-0 ml-auto font-weight-bold text-secondary primary-font line-height-30">{{ \App\Models\Customer::count() }}</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-                            <div class="card border mb-0">
-                                <div class="card-body">
-                                    <div class="d-flex align-items-center mb-3">
-                                        <div>
-                                            <div class="icon-block icon-block-sm bg-info icon-block-floating mr-2">
-                                                <i class="fa fa-p"></i>
-                                            </div>
-                                        </div>
-                                        <span class="font-size-13">محصولات</span>
-                                        <h2 class="mb-0 ml-auto font-weight-bold text-info primary-font line-height-30">{{ \App\Models\Product::count() }}</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-                            <div class="card border mb-0">
-                                <div class="card-body">
-                                    <div class="d-flex align-items-center mb-3">
-                                        <div>
-                                            <div class="icon-block icon-block-sm bg-primary icon-block-floating mr-2">
-                                                <i class="fa fa-shopping-cart"></i>
-                                            </div>
-                                        </div>
-                                        <span class="font-size-13">سفارش مشتری</span>
-                                        <h2 class="mb-0 ml-auto font-weight-bold text-primary primary-font line-height-30">{{ \App\Models\Invoice::where('status','!=','invoiced')->count() }}</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        @can('accountant')
-                            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-                                <div class="card border mb-0">
-                                    <div class="card-body">
-                                        <div class="d-flex align-items-center mb-3">
-                                            <div>
-                                                <div class="icon-block icon-block-sm bg-primary icon-block-floating mr-2">
-                                                    <i class="fa fa-shopping-cart"></i>
-                                                </div>
-                                            </div>
-                                            <span class="font-size-13">فاکتور</span>
-                                            <h2 class="mb-0 ml-auto font-weight-bold text-primary primary-font line-height-30">{{ \App\Models\Invoice::where('status','invoiced')->count() }}</h2>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        @endcan
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-                            <div class="card border mb-0">
-                                <div class="card-body">
-                                    <div class="d-flex align-items-center mb-3">
-                                        <div>
-                                            <div class="icon-block icon-block-sm bg-danger icon-block-floating mr-2">
-                                                <i class="fa fa-cube"></i>
-                                            </div>
-                                        </div>
-
-                                        <span class="font-size-13">بسته های ارسالی</span>
-                                        <h2 class="mb-0 ml-auto font-weight-bold text-danger primary-font line-height-30">{{ \App\Models\Packet::count() }}</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    </div>
                 </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                    <div class="card border mb-0">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <div>
+                                    <div class="icon-block icon-block-sm bg-info icon-block-floating mr-2">
+                                        <i class="fa fa-p"></i>
+                                    </div>
+                                </div>
+                                <span class="font-size-13">محصولات</span>
+                                <h2 class="mb-0 ml-auto font-weight-bold text-info primary-font line-height-30">{{ \App\Models\Product::count() }}</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                    <div class="card border mb-0">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <div>
+                                    <div class="icon-block icon-block-sm bg-primary icon-block-floating mr-2">
+                                        <i class="fa fa-shopping-cart"></i>
+                                    </div>
+                                </div>
+                                <span class="font-size-13">سفارش مشتری</span>
+                                <h2 class="mb-0 ml-auto font-weight-bold text-primary primary-font line-height-30">{{ \App\Models\Invoice::where('status','!=','invoiced')->count() }}</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @can('accountant')
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                        <div class="card border mb-0">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center mb-3">
+                                    <div>
+                                        <div class="icon-block icon-block-sm bg-primary icon-block-floating mr-2">
+                                            <i class="fa fa-shopping-cart"></i>
+                                        </div>
+                                    </div>
+                                    <span class="font-size-13">فاکتور</span>
+                                    <h2 class="mb-0 ml-auto font-weight-bold text-primary primary-font line-height-30">{{ \App\Models\Invoice::where('status','invoiced')->count() }}</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endcan
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                    <div class="card border mb-0">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <div>
+                                    <div class="icon-block icon-block-sm bg-danger icon-block-floating mr-2">
+                                        <i class="fa fa-cube"></i>
+                                    </div>
+                                </div>
+
+                                <span class="font-size-13">بسته های ارسالی</span>
+                                <h2 class="mb-0 ml-auto font-weight-bold text-danger primary-font line-height-30">{{ \App\Models\Packet::count() }}</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <div class="card">
@@ -722,4 +722,3 @@
         });
     </script>
 @endsection
-
