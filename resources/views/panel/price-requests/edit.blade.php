6bd24930d4cc0fca94be62a21264399643fbd8d2
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-08-15 09:56:19 +0330

    fixed some bugs

--- resources/views/panel/price-requests/edit.blade.php
+++ resources/views/panel/price-requests/edit.blade.php
@@ -42,7 +42,6 @@
                                 </tr>
                             @endforeach
                             </tbody>
-
                             <tfoot>
                             <tr></tr>
                             </tfoot>
