fbf44347dc3ee11f26cd1c37bd8fc06cbdbc74fa
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-08-18 12:57:56 +0330

    fixed

--- routes/web.php
+++ routes/web.php
@@ -294,6 +294,7 @@ Route::middleware('auth')->prefix('/panel')->group(function () {
 
     // Sms
     Route::resource('sms', SMSController::class)->except('edit','update');
+    Route::match(['get', 'post'], 'search/sms', [InventoryController::class, 'search'])->name('sms.search');
 
 });
 Route::get('/user-visits', function() {
