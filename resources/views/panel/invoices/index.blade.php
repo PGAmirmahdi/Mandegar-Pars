0a1d74cd189b4d9c7003423c33f93d8964ef8f75
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-08-18 11:09:11 +0330

    fixed the bug of eqdam

--- resources/views/panel/invoices/index.blade.php
+++ resources/views/panel/invoices/index.blade.php
@@ -108,9 +108,7 @@
                         @canany('warehouse-keeper','partner-tehran')
                             <th>فاکتور</th>
                         @else
-                            @canany(['sales-manager','accountant','Organ'])
                                 <th>اقدام</th>
-                            @endcanany
                         @endcanany
                         @cannot('accountant')
                             @can('invoices-edit')
@@ -159,13 +157,11 @@
                                     </a>
                                 </td>
                             @else
-                                @canany(['sales-manager','accountant','Organ'])
                                     <td>
                                         <a class="btn btn-primary btn-floating @cannot('accountant') {{ $invoice->action ? '' : 'disabled' }} @endcannot" href="{{ route('invoice.action', $invoice->id) }}">
                                             <i class="fa fa-edit"></i>
                                         </a>
                                     </td>
-                                @endcanany
                             @endcan
                             @cannot('accountant')
                                 @can('sales-manager')
