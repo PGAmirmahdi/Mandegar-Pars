9ae1aa25f61f3cffe0e5ae917c0e67690510e8a0
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-08-21 09:33:47 +0330

    fixed bug of price history

--- app/Models/PriceHistory.php
+++ app/Models/PriceHistory.php
@@ -16,6 +16,8 @@ class PriceHistory extends Model
         'partner_price_tehran' => 'قیمت همکار - تهران',
         'partner_price_other' => 'قیمت همکار - شهرستان',
         'single_price' => 'قیمت تک فروشی',
+        'domestic_price' => 'قیمت داخلی',
+        'market_price'=>'قیمت بازار',
     ];
 
     public function product()
