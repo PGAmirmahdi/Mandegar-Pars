017ae603e7664197f7220bfbd6e3df4f7f72911c
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-08-29 11:29:40 +0330

    fixed

--- resources/views/panel/layouts/sidebar.blade.php
+++ resources/views/panel/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <!-- begin::navigation -->
 <div class="navigation">
-    <div class="navigation-icon-menu" style="overflow-y: auto; @if(auth()->user()->family == 'رسولی') background-color: #d777ff; @endif">
+    <div class="navigation-icon-menu" style="overflow-y: auto; @if(auth()->user()->family == 'رسولی') background-color: #dba6fc !important; @endif">
         <ul>
             <li class="{{ active_sidebar(['panel','users','users/create','users/{user}/edit','roles','roles/create','roles/{role}/edit', 'tasks','tasks/create','tasks/{task}/edit', 'tasks/{task}', 'notes','notes/create','notes/{note}/edit','leaves','leaves/create','leaves/{leave}/edit','reports','reports/create','reports/{report}/edit','software-updates','software-updates/create','software-updates/{software_update}/edit']) ? 'active' : '' }}" data-toggle="tooltip" title="داشبورد">
                 <a href="#navigationDashboards" title="داشبوردها">
