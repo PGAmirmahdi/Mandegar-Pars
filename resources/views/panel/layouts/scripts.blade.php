4aa02d9085f1fd1b16fb4909bb8a179421a94937
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-09-30 10:41:20 +0330

    fixed

--- resources/views/panel/layouts/scripts.blade.php
+++ resources/views/panel/layouts/scripts.blade.php
@@ -1,4 +1,132 @@
 <script>
+
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher("ac8ae105709d7299a673", {
+        cluster: 'ap1'
+    });
+
+    var channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function(data) {
+        alert(JSON.stringify(data));
+    });
+</script>
+<script>
+    {{-- ajax setup --}}
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    {{-- end ajax setup --}}
+
+    {{-- delete tables row --}}
+    $(document).on('click','.trashRow', function() {
+        let self = $(this)
+        Swal.fire({
+            title: 'حذف شود؟',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#e04b4b',
+            confirmButtonText: 'حذفش کن',
+            cancelButtonText: 'لغو',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: self.data('url'),
+                    type: 'post',
+                    data: {
+                        id: self.data('id'),
+                        _method: 'delete'
+                    },
+                    success: function(res) {
+                        $('tbody:not(.internal_tels)').html($(res).find('tbody:not(.internal_tels)').html());
+                        Swal.fire({
+                            title: 'با موفقیت حذف شد',
+                            icon: 'success',
+                            showConfirmButton: false,
+                            toast: true,
+                            timer: 2000,
+                            timerProgressBar: true,
+                            position: 'top-start',
+                            customClass: {
+                                popup: 'my-toast',
+                                icon: 'icon-center',
+                                title: 'left-gap',
+                                content: 'left-gap',
+                            }
+                        })
+                    },
+                    error: function (jqXHR, exception) {
+                        Swal.fire({
+                            title: jqXHR.responseText,
+                            icon: 'error',
+                            showConfirmButton: false,
+                            toast: true,
+                            timer: 4000,
+                            timerProgressBar: true,
+                            position: 'top-start',
+                            customClass: {
+                                popup: 'my-toast',
+                                icon: 'icon-center',
+                                title: 'left-gap',
+                                content: 'left-gap',
+                            }
+                        })
+                    }
+                })
+
+            }
+        })
+    })
+    {{-- end delete tables row --}}
+
+    //  network status
+    window.addEventListener("offline", (event) => {
+        $('#network_sec').html(`
+                <span data-toggle="tooltip" data-placement="bottom" data-original-title="connecting">
+                    <i class="fa fa-wifi text-danger zoom-in-out"></i>
+                </span>`)
+        $('#network_sec span').tooltip();
+    });
+
+    window.addEventListener("online", (event) => {
+        $('#network_sec').html(`
+                <span data-toggle="tooltip" data-placement="bottom" data-original-title="connected">
+                    <i class="fa fa-wifi text-success"></i>
+                </span>`)
+        $('#network_sec span').tooltip();
+    });
+    // end network status
+    console.log(window.Echo);
+    var audio = new Audio('/audio/notification.wav');
+    let userId = "{{ auth()->id() }}";
+    Echo.join('presence-notification.' + userId)
+        .listen('SendMessage', (e) => {
+            $('#notification_sec a').addClass('nav-link-notify');
+            $('#notif_count').html(parseInt($('#notif_count').html()) + 1);
+            $(".timeline").prepend(`<div class="timeline-item">
+                <div>
+                    <figure class="avatar avatar-state-danger avatar-sm m-r-15 bring-forward">
+                        <span class="avatar-title bg-primary-bright text-primary rounded-circle">
+                            <i class="fa fa-bell font-size-20"></i>
+                        </span>
+                    </figure>
+                </div>
+                <div>
+                    <p class="m-b-5">
+                        <a href="/panel/read-notifications/${e.data.id}">${e.data.message}</a>
+                    </p>
+                    <small class="text-muted">
+                        <i class="fa fa-clock-o m-r-5"></i>الان
+                    </small>
+                </div>
+            </div>`);
+            audio.play();
+        });
+</script>
+<script>
     // firebase push notification
     var firebaseConfig = {
         apiKey: "AIzaSyCUdU7PnQmzrkcJDFOJsIGcpe7CZV1GBrA",
@@ -136,131 +264,3 @@
 
 
 <script src="https://www.gstatic.com/firebasejs/7.23.0/firebase.js"></script>
-<script>
-
-    // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
-
-    var pusher = new Pusher("ac8ae105709d7299a673", {
-        cluster: 'ap1'
-    });
-
-    var channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data) {
-        alert(JSON.stringify(data));
-    });
-</script>
-<script>
-    {{-- ajax setup --}}
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-    {{-- end ajax setup --}}
-
-    {{-- delete tables row --}}
-    $(document).on('click','.trashRow', function() {
-        let self = $(this)
-        Swal.fire({
-            title: 'حذف شود؟',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#e04b4b',
-            confirmButtonText: 'حذفش کن',
-            cancelButtonText: 'لغو',
-        }).then((result) => {
-            if (result.isConfirmed) {
-                $.ajax({
-                    url: self.data('url'),
-                    type: 'post',
-                    data: {
-                        id: self.data('id'),
-                        _method: 'delete'
-                    },
-                    success: function(res) {
-                        $('tbody:not(.internal_tels)').html($(res).find('tbody:not(.internal_tels)').html());
-                        Swal.fire({
-                            title: 'با موفقیت حذف شد',
-                            icon: 'success',
-                            showConfirmButton: false,
-                            toast: true,
-                            timer: 2000,
-                            timerProgressBar: true,
-                            position: 'top-start',
-                            customClass: {
-                                popup: 'my-toast',
-                                icon: 'icon-center',
-                                title: 'left-gap',
-                                content: 'left-gap',
-                            }
-                        })
-                    },
-                    error: function (jqXHR, exception) {
-                        Swal.fire({
-                            title: jqXHR.responseText,
-                            icon: 'error',
-                            showConfirmButton: false,
-                            toast: true,
-                            timer: 4000,
-                            timerProgressBar: true,
-                            position: 'top-start',
-                            customClass: {
-                                popup: 'my-toast',
-                                icon: 'icon-center',
-                                title: 'left-gap',
-                                content: 'left-gap',
-                            }
-                        })
-                    }
-                })
-
-            }
-        })
-    })
-    {{-- end delete tables row --}}
-
-    //  network status
-    window.addEventListener("offline", (event) => {
-        $('#network_sec').html(`
-                <span data-toggle="tooltip" data-placement="bottom" data-original-title="connecting">
-                    <i class="fa fa-wifi text-danger zoom-in-out"></i>
-                </span>`)
-        $('#network_sec span').tooltip();
-    });
-
-    window.addEventListener("online", (event) => {
-        $('#network_sec').html(`
-                <span data-toggle="tooltip" data-placement="bottom" data-original-title="connected">
-                    <i class="fa fa-wifi text-success"></i>
-                </span>`)
-        $('#network_sec span').tooltip();
-    });
-    // end network status
-    console.log(window.Echo);
-    var audio = new Audio('/audio/notification.wav');
-    let userId = "{{ auth()->id() }}";
-    Echo.join('presence-notification.' + userId)
-        .listen('SendMessage', (e) => {
-            $('#notification_sec a').addClass('nav-link-notify');
-            $('#notif_count').html(parseInt($('#notif_count').html()) + 1);
-            $(".timeline").prepend(`<div class="timeline-item">
-                <div>
-                    <figure class="avatar avatar-state-danger avatar-sm m-r-15 bring-forward">
-                        <span class="avatar-title bg-primary-bright text-primary rounded-circle">
-                            <i class="fa fa-bell font-size-20"></i>
-                        </span>
-                    </figure>
-                </div>
-                <div>
-                    <p class="m-b-5">
-                        <a href="/panel/read-notifications/${e.data.id}">${e.data.message}</a>
-                    </p>
-                    <small class="text-muted">
-                        <i class="fa fa-clock-o m-r-5"></i>الان
-                    </small>
-                </div>
-            </div>`);
-            audio.play();
-        });
-</script>
