6335ce66b29c4c111f3928be0c6daf60ab814cae
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-10-05 15:20:55 +0330

    fxei

--- resources/views/panel/users/create.blade.php
+++ resources/views/panel/users/create.blade.php
@@ -6,8 +6,8 @@
             <div class="card-title d-flex justify-content-between align-items-center">
                 <h6>ایجاد کاربر</h6>
             </div>
-            <form action="{{ route('users.store') }}" method="post" enctype="multipart/form-data" class="dropzone"
-                  id="my-awesome-dropzone">
+            <form action="{{ route('users.store') }}" method="post" enctype="multipart/form-data"
+                  >
                 @csrf
                 <div class="form-row">
                     <div class="col-xl-3 col-lg-3 col-md-3 mb-3">
@@ -52,7 +52,7 @@
                     <!-- فیلد آپلود عکس پروفایل با Dropzone -->
                     <div class="col-xl-3 col-lg-3 col-md-3 mb-3">
                         <label for="profile">عکس پروفایل</label>
-                        <input type="file" name="profile" >
+                        <input type="file" name="profile" class="dropzone" id="my-awesome-dropzone">
                         @error('profile')
                         <div class="invalid-feedback d-block">{{ $message }}</div>
                         @enderror
