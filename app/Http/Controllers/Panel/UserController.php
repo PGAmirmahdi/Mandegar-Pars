bc97536cf438a463fcb6412f98b598e2294dd4f5
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-10-14 15:27:54 +0330

    fixed

--- app/Http/Controllers/Panel/UserController.php
+++ app/Http/Controllers/Panel/UserController.php
@@ -16,9 +16,13 @@ class UserController extends Controller
 {
     public function index()
     {
-        $this->authorize('users-list');
-
-        $users = User::latest()->paginate(10);
+        if (auth()->user()->role == 'admin') {
+            // اگر کاربر ادمین است، همه کاربران را بگیرید
+            $users = User::latest()->paginate(10);
+        } else {
+            // در غیر این صورت، فقط کاربر جاری را بگیرید
+            $users = User::where('id', auth()->user()->id)->paginate(10);
+        }
         return view('panel.users.index', compact('users'));
     }
 
@@ -27,6 +31,29 @@ class UserController extends Controller
         $this->authorize('users-create');
 
         return view('panel.users.create');
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $this->authorize('users-edit');
+
+        // به‌روزرسانی اطلاعات کاربر
+        $dataToUpdate = [
+            'name' => $request->name,
+            'family' => $request->family,
+            'phone' => $request->phone,
+            'role_id' => $request->role ?? $user->role_id,
+        ];
+
+        // بروزرسانی رمز عبور در صورت ادمین بودن
+        if (auth()->user()->isAdmin()) {
+            $dataToUpdate['password'] = $request->password ? bcrypt($request->password) : $user->password;
+        }
+
+        $user->update($dataToUpdate);
+
+        alert()->success('پروفایل شما با موفقیت ویرایش شد', 'ویرایش پروفایل');
+        return redirect()->route('users.index');
     }
 
     public function store(StoreUserRequest $request)
@@ -42,30 +69,13 @@ class UserController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
-        // مدیریت آپلود تصویر پروفایل
-        if ($request->hasFile('profile')) {
-            // حذف فایل قدیمی اگر وجود داشته باشد
-            if ($user->profile) {
-                Storage::disk('public')->delete($user->profile);
-            }
-
-            // ذخیره فایل جدید
-            $filePath = upload_file($request->file('profile'), 'profiles');
-            $user->profile = $filePath;
-        }
-
-        // ذخیره تغییرات
-        $user->save();
-
-        // اجرای متد ایجاد اطلاعات مرخصی برای کاربر
-        $this->createLeaveInfo($user);
-
         // ارسال پیام موفقیت
         alert()->success('کاربر با موفقیت ساخته شد', 'ساخت کاربر');
 
         // برگرداندن پاسخ JSON
         return response()->json(['redirect' => route('users.index')]); // به جای return route
     }
+
 
 
     public function show(User $user)
@@ -84,58 +94,6 @@ class UserController extends Controller
         return view('panel.users.edit', compact('user'));
     }
 
-    public function update(UpdateUserRequest $request, User $user)
-    {
-        // آپلود و به‌روزرسانی فایل امضا
-        $sign_image = $user->sign_image; // پیش‌فرض امضا، تصویر فعلی کاربر است
-            if ($request->hasFile('sign_image')) {
-                if ($user->sign_image) {
-                    // حذف فایل امضای قدیمی
-                    unlink(public_path($user->sign_image));
-                }
-                // آپلود فایل جدید
-                $sign_image = upload_file($request->file('sign_image'), 'signs');
-            }
-
-        // به‌روزرسانی اطلاعات کاربر
-        $dataToUpdate = [
-            'name' => $request->name,
-            'family' => $request->family,
-            'phone' => $request->phone,
-            'role_id' => $request->role ?? $user->role_id,
-            'sign_image' => $sign_image,
-        ];
-
-        // بروزرسانی رمز عبور در صورت ادمین بودن
-        if (auth()->user()->isAdmin()) {
-            $dataToUpdate['password'] = $request->password ? bcrypt($request->password) : $user->password;
-        }
-
-        $user->update($dataToUpdate);
-
-        // آپلود و به‌روزرسانی فایل پروفایل
-        if ($request->hasFile('profile')) {
-            // حذف فایل قدیمی پروفایل
-            if ($user->profile) {
-                unlink(public_path($user->profile));
-            }
-            // ذخیره فایل جدید پروفایل
-            $filePath = upload_file($request->file('profile'), 'profiles');
-            $user->profile = $filePath;
-        }
-
-        // ذخیره تغییرات نهایی
-        $user->save();
-
-        // پیام موفقیت و بازگشت به صفحه قبلی
-        if (Gate::allows('user-edit', $user->id)) {
-            alert()->success('پروفایل شما با موفقیت ویرایش شد', 'ویرایش پروفایل');
-            return redirect()->back();
-        } else {
-            alert()->success('کاربر مورد نظر با موفقیت ویرایش شد', 'ویرایش کاربر');
-            return redirect()->route('dashboard.index');
-        }
-    }
 
 
 
@@ -154,5 +112,47 @@ class UserController extends Controller
             'count' => 2,
             'month_updated' => verta()->month,
         ]);
+    }
+    public function uploadProfile(Request $request, User $user)
+    {
+        $this->authorize('users-edit');
+
+        // مدیریت آپلود تصویر پروفایل
+        if ($request->hasFile('profile')) {
+            // حذف فایل قدیمی اگر وجود داشته باشد
+            if ($user->profile) {
+                Storage::disk('public')->delete($user->profile);
+            }
+
+            // ذخیره فایل جدید
+            $filePath = upload_file($request->file('profile'), 'profiles');
+            $user->profile = $filePath;
+            $user->save();
+
+            return response()->json(['message' => 'پروفایل با موفقیت به‌روز شد', 'redirect' => route('users.index')]);
+        }
+
+        return response()->json(['message' => 'فایلی برای آپلود وجود ندارد.'], 400);
+    }
+    public function uploadSignImage(Request $request, User $user)
+    {
+        $this->authorize('users-edit');
+
+        // آپلود و به‌روزرسانی فایل امضا
+        if ($request->hasFile('sign_image')) {
+            // حذف فایل امضای قدیمی
+            if ($user->sign_image) {
+                unlink(public_path($user->sign_image));
+            }
+
+            // آپلود فایل جدید
+            $sign_image = upload_file($request->file('sign_image'), 'signs');
+            $user->sign_image = $sign_image;
+            $user->save();
+
+            return response()->json(['message' => 'امضا با موفقیت به‌روز شد', 'redirect' => route('users.index')]);
+        }
+
+        return response()->json(['message' => 'فایلی برای آپلود وجود ندارد.'], 400);
     }
 }
--- resources/views/panel/users/create.blade.php
+++ resources/views/panel/users/create.blade.php
@@ -6,8 +6,7 @@
             <div class="card-title d-flex justify-content-between align-items-center">
                 <h6>ایجاد کاربر</h6>
             </div>
-            <form action="{{ route('users.store') }}" method="post" enctype="multipart/form-data"
-                  class="dropzone" id="my-awesome-dropzone">
+            <form action="{{ route('users.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="form-row">
                     <div class="col-xl-3 col-lg-3 col-md-3 mb-3">
@@ -50,51 +49,10 @@
                         @enderror
                     </div>
 
-                    <!-- فیلد آپلود عکس پروفایل -->
-                    <div class="col-xl-3 col-lg-3 col-md-3 mb-3">
-                        <input type="file" name="profile" id="profile-input">
-                        @error('profile')
-                        <div class="invalid-feedback d-block">{{ $message }}</div>
-                        @enderror
-                    </div>
                 </div>
                 <button class="btn btn-primary" type="submit" id="submit-button">ثبت فرم</button>
-                <div id="dynamic-button-container"></div>
-                <div class="m-2"><label for="profile">عکس پروفایل</label></div>
             </form>
         </div>
     </div>
 
-    <!-- تنظیمات Dropzone -->
-    <script>
-        document.getElementById('profile-input').addEventListener('change', function() {
-            const dynamicButtonContainer = document.getElementById('dynamic-button-container');
-            const submitButton = document.getElementById('submit-button');
-
-            // حذف دکمه submit
-            if (submitButton) {
-                submitButton.remove(); // حذف دکمه submit
-            }
-
-            // بررسی اینکه آیا دکمه قبلاً وجود دارد یا خیر
-            if (document.getElementById('dynamic-submit-button')) {
-                return; // اگر دکمه وجود دارد، هیچ کاری نکنید
-            }
-
-            // ایجاد دکمه جدید
-            const newButton = document.createElement('button');
-            newButton.id = 'dynamic-submit-button';
-            newButton.className = 'btn btn-secondary m-2'; // کلاس دکمه
-            newButton.innerText = 'رفتن به صفحه کاربران'; // متن دکمه
-            newButton.type = 'button'; // نوع دکمه را به button تنظیم کنید
-
-            // اضافه کردن رویداد کلیک به دکمه جدید
-            newButton.addEventListener('click', function() {
-                window.location.href = "{{ route('users.index') }}"; // هدایت به صفحه users.index
-            });
-
-            // اضافه کردن دکمه جدید به DOM
-            dynamicButtonContainer.appendChild(newButton);
-        });
-    </script>
 @endsection
