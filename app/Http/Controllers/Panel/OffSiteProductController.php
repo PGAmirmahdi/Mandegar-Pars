7207a1774443c8251ae2876ed09511c79e26f5c0
Amirmahdi <pgamirmahdi@gmail.com>
2024-12-31 14:35:28 +0330

    fixed

--- app/Http/Controllers/Panel/OffSiteProductController.php
+++ app/Http/Controllers/Panel/OffSiteProductController.php
@@ -235,8 +235,12 @@ OffSiteProductController extends Controller
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Language: en-US,en;q=0.9',
+            'Connection: keep-alive',
+        ]);
         $response = curl_exec($ch);
         curl_close($ch);
         // لاگ کردن پاسخ
