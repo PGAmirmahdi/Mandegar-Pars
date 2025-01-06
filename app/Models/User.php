19afa949d410aac8efe6e94ec5b77c3378c7bd28
Amirmahdi <pgamirmahdi@gmail.com>
2025-01-06 10:56:51 +0330

    fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,7 +71,7 @@ class User extends Authenticatable
     }
     public function isOfficeManager()
     {
-        return $this->role->name == 'office-manager';
+        return $this->role->permissions->pluck('name')->contains('OfficeManager');
     }
     public function isPartnerCity()
     {
