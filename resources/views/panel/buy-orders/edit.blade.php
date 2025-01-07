4ca82cede2915328fd348afe66a751dbb69cf074
Amirmahdi <pgamirmahdi@gmail.com>
2025-01-07 16:22:14 +0330

    fixed

--- resources/views/panel/buy-orders/edit.blade.php
+++ resources/views/panel/buy-orders/edit.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'ویرایش سفارش خرید')
 @section('styles')
     <style>
-        table tbody tr td input {
+        table tbody tr td input{
             text-align: center;
         }
     </style>
@@ -41,40 +41,25 @@
                                     <tr>
                                         <td>
                                             <select class="js-example-basic-single" name="products[]" required>
-                                                <option value="" disabled>انتخاب کنید</option>
-                                                @foreach($products as $product)
-                                                    <option value="{{ $product->id }}" {{ $product->id == $item->product ? 'selected' : '' }}>
-                                                        {{ $product->category->name . ' - ' . $product->title . ' - ' . $product->productModels->slug }}
+                                                <option value="" disabled selected>انتخاب کنید</option>
+                                                @foreach($products as $item)
+                                                    <option
+                                                        value="{{ $item->id }}"
+                                                        {{ isset($productId) && $item->id == $productId ? 'selected' : '' }}>
+                                                        {{ $item->category->name . ' - ' . $item->title . ' - ' . $item->productModels->slug }}
                                                     </option>
                                                 @endforeach
                                             </select>
                                         </td>
-                                        <td>
-                                            <input type="number" class="form-control" name="counts[]" min="1" value="{{ $item->count }}" required>
-                                        </td>
-                                        <td>
-                                            <button type="button" class="btn btn-danger btn-floating btn_remove"><i class="fa fa-trash"></i></button>
-                                        </td>
+                                        <td><input type="number" class="form-control" name="counts[]" min="1" value="{{ $item->count }}" required></td>
+                                        <td><button type="button" class="btn btn-danger btn-floating btn_remove"><i class="fa fa-trash"></i></button></td>
                                     </tr>
                                 @endforeach
                             @else
                                 <tr>
-                                    <td>
-                                        <select class="js-example-basic-single" name="products[]" required>
-                                            <option value="" disabled selected>انتخاب کنید</option>
-                                            @foreach($products as $product)
-                                                <option value="{{ $product->id }}">
-                                                    {{ $product->category->name . ' - ' . $product->title . ' - ' . $product->productModels->slug }}
-                                                </option>
-                                            @endforeach
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <input type="number" class="form-control" name="counts[]" min="1" value="1" required>
-                                    </td>
-                                    <td>
-                                        <button type="button" class="btn btn-danger btn-floating btn_remove"><i class="fa fa-trash"></i></button>
-                                    </td>
+                                    <td><input type="text" class="form-control" name="products[]" placeholder="HP 05A" required></td>
+                                    <td><input type="number" class="form-control" name="counts[]" min="1" value="1" required></td>
+                                    <td><button type="button" class="btn btn-danger btn-floating btn_remove"><i class="fa fa-trash"></i></button></td>
                                 </tr>
                             @endif
                             </tbody>
@@ -100,30 +85,29 @@
             $(document).on('click', '#btn_add', function () {
                 $('table tbody').append(`
                     <tr>
-                        <td>
-                            <select class="js-example-basic-single" name="products[]" required>
-                                <option value="" disabled selected>انتخاب کنید</option>
-                                @foreach($products as $product)
-                <option value="{{ $product->id }}">
-                                        {{ $product->category->name . ' - ' . $product->title . ' - ' . $product->productModels->slug }}
+                       <td>
+                                            <select class="js-example-basic-single" name="products[]" required>
+                                                <option value="" disabled selected>انتخاب کنید</option>
+                                                @foreach($products as $item)
+                <option
+                    value="{{ $item->id }}"
+                                                        {{ isset($productId) && $item->id == $productId ? 'selected' : '' }}>
+                                                        {{ $item->category->name . ' - ' . $item->title . ' - ' . $item->productModels->slug }}
                 </option>
 @endforeach
                 </select>
             </td>
-            <td>
-                <input type="number" class="form-control" name="counts[]" min="1" value="1" required>
-            </td>
-            <td>
-                <button type="button" class="btn btn-danger btn-floating btn_remove"><i class="fa fa-trash"></i></button>
-            </td>
-        </tr>
-`);
-            });
+<td><input type="number" class="form-control" name="counts[]" min="1" value="1" required></td>
+<td><button type="button" class="btn btn-danger btn-floating btn_remove"><i class="fa fa-trash"></i></button></td>
+</tr>
+`)
+            })
 
             // remove item
             $(document).on('click', '.btn_remove', function () {
-                $(this).parent().parent().remove();
-            });
-        });
+                $(this).parent().parent().remove()
+            })
+        })
     </script>
 @endsection
+
