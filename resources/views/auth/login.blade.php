ac1837babadb2bcf65994d573a6def82bf264212
Amirmahdi <pgamirmahdi@gmail.com>
2025-01-25 17:27:44 +0330

    delete button in analyse

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,93 +20,217 @@
     <link rel="stylesheet" href="assets/css/app.css" type="text/css">
 
     <link rel="manifest" href="/manifest.json">
-    <!-- Najva Push Notification -->
-    <script type="text/javascript">
-        (function () {
-            var now = new Date();
-            var version = now.getFullYear().toString() + "0" + now.getMonth() + "0" + now.getDate() +
-                "0" + now.getHours();
-            var head = document.getElementsByTagName("head")[0];
-            var link = document.createElement("link");
-            link.rel = "stylesheet";
-            link.href = "https://van.najva.com/static/cdn/css/local-messaging.css" + "?v=" + version;
-            head.appendChild(link);
-            var script = document.createElement("script");
-            script.type = "text/javascript";
-            script.async = true;
-            script.src = "https://van.najva.com/static/js/scripts/new-website436970-website-54287-1faec3c1-6f27-4881-b219-5f5b5737f31b.js" + "?v=" + version;
-            head.appendChild(script);
-        })()
-    </script>
-    <!-- END NAJVA PUSH NOTIFICATION -->
-    <script type="module" src="https://cdn.jsdelivr.net/npm/@friendlycaptcha/sdk@0.1.8/site.min.js" async
-            defer></script>
-    <script nomodule src="https://cdn.jsdelivr.net/npm/@friendlycaptcha/sdk@0.1.8/site.compat.min.js" async
-            defer></script>
 </head>
+
 <style>
-    #captcha_sec img {
-        cursor: pointer;
+    body {
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        .form-wrapper{
+            background-color: transparent !important;
+        }
     }
 
-    #captcha_sec input {
-        text-align: center !important;
-        letter-spacing: 1rem;
+    .video-background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        z-index: -1;
     }
-    iframe {
-        width: 100%; /* عرض کامل */
-        height: 80px; /* ارتفاع مورد نظر */
-        margin:0px;
-        position:relative !important;
+
+    .logo {
+        margin-left: 20px;
+        flex-shrink: 0;
+    }
+
+    .logo img {
+        width: 220px;
+    }
+
+    form {
+        background-color: transparent; /* حذف رنگ پس‌زمینه */
+        width: 100%;
+        height: 100%;
+    }
+
+    .form-container {
+        margin-right: 20px;
+        width: 500px;
+    }
+
+    .form-wrapper {
+        margin-top: 100px;
+        padding: 0px !important;
+        opacity: 0;
+        transition: opacity 1s;
+        display: flex;
+        flex-direction: row; /* جهت افقی برای فرم و تصویر */
+        justify-content: space-between; /* فاصله مناسب بین فرم و تصویر */
+        align-items: center; /* ترازبندی عمودی */
+        background-color: rgba(76, 26, 122, 0.2); /* رنگ با شفافیت */
+        backdrop-filter: blur(15px); /* افکت blur */
+        -webkit-backdrop-filter: blur(15px); /* پشتیبانی از مرورگرهای وب‌کیت */
+        border-radius: 15px; /* گوشه‌های گرد */
+        box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2); /* سایه */
+        width: 700px !important;
+    }
+
+    .form-container {
+        padding: 50px !important;
+        display: flex;
+        flex-direction: column;
+        align-items: center; /* تنظیم فرم در مرکز عمودی */
+        justify-content: center;
+        width: 50%; /* عرض فرم */
+    }
+
+    .form-wrapper.slide-down {
+        animation: slideDown 1.5s ease forwards;
+    }
+
+    @keyframes slideDown {
+        0% {
+            transform: translateY(20%);
+            opacity: 0;
+        }
+        100% {
+            transform: translateY(0%);
+            opacity: 1;
+        }
+    }
+
+    .form-group {
+        position: relative;
+        margin-bottom: 20px;
+    }
+
+    .form-group label {
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 10px;
+        transform: translateY(-50%);
+        color: #999;
+        transition: all 0.3s ease;
+        pointer-events: none;
+    }
+
+    .form-group input {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        font-size: 14px;
+        outline: none;
+        transition: all 0.3s ease;
+    }
+
+    .right{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 400px !important;
+    }
+
+    .form-group input:focus {
+        border-color: #5d4a9c;
+    }
+
+    .form-group input:focus + label,
+    .form-group input:not(:placeholder-shown) + label {
+        top: -10px;
+        font-size: 12px;
+        color: #5d4a9c;
+    }
+
+    .form-group input:focus + label {
+        font-weight: bold;
+    }
+
+    /* تغییرات برای صفحات موبایل */
+    @media (max-width: 768px) {
+        .form-wrapper {
+            flex-direction: column; /* چیدمان عمودی فرم و تصویر */
+            align-items: center; /* وسط چین کردن */
+            width: 100% !important;
+        }
+
+        .right {
+            display: none; /* مخفی کردن تصویر سمت راست در صفحه موبایل */
+        }
+
+        .form-container {
+            width: 100%; /* عرض فرم به 100% برای صفحه موبایل */
+            padding: 20px; /* کاهش فاصله در موبایل */
+        }
+
+        .form-group label {
+            color: #fff !important; /* تغییر رنگ لیبل‌ها به سفید */
+        }
+
+        .video-background {
+            object-fit: cover; /* کشیدن ویدیو به اندازه صفحه */
+        }
+
+        .form-wrapper {
+            margin-top: 20px; /* کاهش فاصله بالای فرم در موبایل */
+            width: 90% !important; /* عرض فرم در موبایل */
+        }
+
+        .form-group input {
+            font-size: 16px; /* افزایش اندازه فونت ورودی در موبایل */
+        }
     }
 </style>
+
 <body class="form-membership">
 
-<!-- begin::page loader-->
-@include('panel.layouts.loader')
-<!-- end::page loader -->
+<video class="video-background" autoplay muted playsinline>
+    <source src="{{ asset('assets/media/login.mp4') }}" type="video/mp4">
+    <img src="assets/media/image/fallback.jpg" alt="ویدیو پشتیبانی نمی‌شود.">
+</video>
 
-<div class="form-wrapper" style="background-color: rgba(255,255,255,0.18);
-            backdrop-filter: blur(10px);
-            -webkit-backdrop-filter: blur(2px);box-shadow:0px 2px 5px 2px gainsboro">
-
-    <!-- logo -->
-    <div class="logo">
-        <img src="assets/media/image/logo-sm.png" alt="image">
+<div class="form-wrapper">
+    <div class="bg-white right">
+        <img src="assets/media/image/login.png" alt="عکس سمت راست"  width="300px">
     </div>
-    <!-- ./ logo -->
-
-    <h5>ورود</h5>
-
-    <!-- form -->
-    <form action="{{ route('login') }}" method="post">
-        @csrf
-        <div class="form-group">
-            <input type="text" name="phone" class="form-control text-left" placeholder="شماره موبایل" dir="ltr" required
-                   autofocus>
-        </div>
-        <div class="form-group">
-            <input type="password" name="password" class="form-control text-left" placeholder="رمز عبور" dir="ltr"
-                   required>
-        </div>
-        <div class="frc-captcha" data-sitekey="{{ env('FRIENDLY_CAPTCHA_SITEKEY') }}" style="width:100vw;"></div>
-        <div class="form-group" id="captcha_sec">
-            @error('frc-captcha-response')
-            <span class="invalid-feedback d-block" role="alert">
+    <div class="form-container">
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            <div>
+                <img src="assets/media/image/header-logo.png" width="250px" alt="لوگو">
+            </div>
+            <h5 class="text-primary">ورود</h5>
+            <div class="form-group">
+                <input type="text" name="phone" id="phone" class="form-control text-left" placeholder="" dir="ltr" required autofocus>
+                <label for="phone">شماره موبایل</label>
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" id="password" class="form-control text-left" placeholder="" dir="ltr" required autocomplete="off">
+                <label for="password">رمز عبور</label>
+            </div>
+            <div class="frc-captcha" data-sitekey="{{ env('FRIENDLY_CAPTCHA_SITEKEY') }}" style="width:100vw;"></div>
+            <div class="form-group" id="captcha_sec">
+                @error('frc-captcha-response')
+                <span class="invalid-feedback d-block" role="alert">
             <strong>{{ $message }}</strong>
             </span>
+                @enderror
+            </div>
+            <button class="btn btn-primary btn-block">ورود</button>
+            @error('phone')
+            <span class="invalid-feedback d-block" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
             @enderror
-        </div>
-        <button class="btn btn-primary btn-block">ورود</button>
-        @error('phone')
-        <span class="invalid-feedback d-block" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-    </form>
-    <!-- ./ form -->
+        </form>
+    </div>
 </div>
-
 
 <!-- Plugin scripts -->
 <script src="vendors/bundle.js"></script>
@@ -115,29 +239,36 @@
 <script src="assets/js/app.js"></script>
 
 <script>
+    setTimeout(() => {
+        const formWrapper = document.querySelector('.form-wrapper');
+        formWrapper.style.opacity = '1';
+        formWrapper.classList.add('slide-down');
+    }, 3000);
+
+    const video = document.querySelector('.video-background');
+    video.addEventListener('ended', () => {
+        video.pause();
+    });
+
     $(document).ready(function () {
-        $(document).on('click', '#captcha_sec img', function () {
+        $('#captcha_sec img').on('click', function () {
             $.ajax({
                 type: 'get',
                 url: '/captcha/api',
                 success: function (res) {
-                    $('#captcha_sec img').attr('src', res.img)
-                    // console.log($(this))
+                    $('#captcha_sec img').attr('src', res.img);
                 }
-            })
-        })
-    })
-    document.querySelector('form').addEventListener('submit', function (event) {
-        const captchaResponse = document.querySelector('input[name="frc-captcha-response"]');
-        if (captchaResponse.value === '.ACTIVATED') {
-            captchaResponse.value = ''; // مقدار را خالی کنید
-        } else if (captchaResponse.value === '.UNACTIVATED') {
-            captchaResponse.value = ''; // مقدار را خالی کنید
-        }
-
+            });
+        });
     });
 
+    document.querySelector('form').addEventListener('submit', function (event) {
+        const captchaResponse = document.querySelector('input[name="frc-captcha-response"]');
+        if (captchaResponse && (captchaResponse.value === '.ACTIVATED' || captchaResponse.value === '.UNACTIVATED')) {
+            captchaResponse.value = '';
+        }
+    });
 </script>
-</body>
 
+</body>
 </html>
