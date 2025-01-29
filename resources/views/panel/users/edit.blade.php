d1a9e9f2ef868534619efe4fdd9146d3853ce0e0
Amirmahdi <pgamirmahdi@gmail.com>
2025-01-29 16:59:25 +0330

    fixed bug in users

--- resources/views/panel/users/edit.blade.php
+++ resources/views/panel/users/edit.blade.php
@@ -113,6 +113,17 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/min/dropzone.min.js"></script>
     <script type="text/javascript">
+        $(document).ready(function () {
+            $('#submit-btn').on('click', function () {
+                let button = $(this);
+
+                // تغییر متن و غیر فعال کردن دکمه
+                button.prop('disabled', true).text('در حال ارسال...');
+
+                // ارسال فرم به صورت خودکار
+                button.closest('form').submit();
+            });
+        });
         Dropzone.autoDiscover = false;
 
         // Dropzone برای آپلود تصویر امضا
