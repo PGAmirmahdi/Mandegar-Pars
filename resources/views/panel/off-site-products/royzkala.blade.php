2ff8cb3c73f52739d2c3123c242c2cbb48191747
mostafa <shrytzadh@gmail.com>
2025-02-11 19:41:36 +0330

    add royzkala to offsite products

---
+++ resources/views/panel/off-site-products/royzkala.blade.php
@@ -0,0 +1,43 @@
+@extends('panel.layouts.master')
+@section('title', 'وبسایت رویز کالا')
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title d-flex justify-content-between align-items-center">
+                <h6>وبسایت رویز کالا</h6>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered dataTable dtr-inline text-center">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>خصوصیات</th>
+                        <th>قیمت اصلی</th>
+                        <th>قیمت <span class="text-success">(30،000+)</span></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($data as $key => $item)
+                        <tr>
+                            <td>{{ ++$key }}</td>
+                            <td>
+                                @foreach($item->attributes as $attribute)
+                                    {{ urldecode($attribute).' ،' }}
+                                @endforeach
+                            </td>
+                            <td>{{ number_format($item->display_price) }} تومان</td>
+                            <td>{{ number_format($item->display_price + 30000) }} تومان</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
